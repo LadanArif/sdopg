6e7b06a8a3627ab9d4caa5766394978d17949489
Ladan <lada0080@stud.kea.dk>
2024-10-11 00:15:21 +0200

    ændringer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "280% 280%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "280% 280%",
+            "background-position": "center right",
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
